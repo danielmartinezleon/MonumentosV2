9a8239b0508cbd18f704ee8a67e2e1e031c73ca8
Daniel Martínez León <daniel1martinezleon@gmail.com>
2024-12-12 14:30:17 +0100

    a

---
+++ src/main/java/com/salesianostriana/dam/monumentosv2/repository/MonumentoRepository.java
@@ -0,0 +1,9 @@
+package com.salesianostriana.dam.monumentosv2.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MonumentoRepository {
+
+
+}
---
+++ src/test/java/com/salesianostriana/dam/monumentosv2/Monumentosv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.monumentosv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Monumentosv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
